b5cd126d0bd82b6fa5a0aedfe033008bdacf4c87
ANIKET SRIVASTAVA <aniket08official@gmail.com>
2024-12-27 20:12:24 +0530

    navbar added !

--- client/src/components/Navbar.jsx/Navbar.jsx
+++ client/src/components/Navbar.jsx/Navbar.jsx
@@ -4,8 +4,10 @@ import { NavLink, useNavigate } from 'react-router-dom'
 import { IoHomeSharp , IoBusiness} from "react-icons/io5";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
+import { MdAdminPanelSettings } from "react-icons/md";
 import './Navbar.css';
 import { useUser } from '../userContext/userContext';
+import WebsiteLogo from '../../assets/webiste_full_logo.png';
 const Navbar = () => {
 
   const { user, setUser } = useUser();
@@ -28,11 +30,12 @@ const Navbar = () => {
 // console.log("user data: " , user);
   return (
     <>
-    <header className="p-4 bg-green-200 text-white flex justify-between items-center">
-    <h1 className='text-black text-xl font-semibold' >{user ? `Welcome, ${user.name.toUpperCase()} to UrbanHaven !` : "Welcome to UrbanHaven !"}</h1>
-    <p className='text-purple-700'>id:{user ?user._id : "Not Logged in yet"}</p>
-    <div>
+    <header className=" bg-yellow-200  h-20 text-white flex justify-between items-center">
+    <h1 className='text-black  ml-10 text-xl font-semibold' >{user ? `Welcome, ${user.name.toUpperCase()} to UrbanHaven !` : "Welcome to UrbanHaven !"}</h1>
+    <NavLink to="/" ><img src={WebsiteLogo} className='w-28 ' /></NavLink>
+    <div className=' mr-5 mt-2'>
     <ul className='flex flex-row justify-end flex-wrap gap-2'>
+    <li><NavLink to="/admin" ><MdAdminPanelSettings className='text-2xl text-black font-bold' /></NavLink></li>
     <li><NavLink to="/" ><IoHomeSharp className='text-2xl text-black font-bold'/></NavLink></li>
     <li><NavLink to="/contact" ><FaMapMarkerAlt  className='text-2xl text-black font-bold'/></NavLink></li>
     <li><NavLink to="/api/v1/about" ><IoBusiness className='text-2xl text-black font-bold' />
@@ -41,7 +44,7 @@ const Navbar = () => {
   
 
       {user ? (
-        <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded-2xl">
+        <button onClick={handleLogout} className="bg-red-500 p-3  rounded-2xl">
           Logout
         </button>
       ) : (
