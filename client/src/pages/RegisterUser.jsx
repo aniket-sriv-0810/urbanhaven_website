362b531161d832be44d8eff5e7285e9e8525fc67
ANIKET SRIVASTAVA <aniket08official@gmail.com>
2025-01-05 12:59:07 +0530

    User Dashboard UI working continued... !

--- client/src/pages/RegisterUser.jsx
+++ client/src/pages/RegisterUser.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React from 'react'
 import { useState  } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import {useUser} from '../components/userContext/userContext';
 import './user/RegisterUser.css'
 import userVideo from '../assets/Sign Up form.mp4';
 import icon from '../assets/icon.png';
+import { FaRegUser } from "react-icons/fa";
 import './RegisterUser.css'
 const RegisterUser = () => {
   const {setUser} = useUser();
@@ -69,22 +70,22 @@ const RegisterUser = () => {
  
   return (
     <>
-   <div className="bg-[url('/assets/auth.png')] bg-cover bg-bottom flex flex-row justify-evenly items-center flex-wrap bg-purple-200 min-h-screen ">
-   <img src={icon} alt="icon" className='w-max '/>
-
-   <div className='flex flex-col border-2 border-white rounded-xl p-6'>
-   <h1 className='text-white text-center font-bold text-xl'>Enter Your Credentials</h1>
+   <div className="bg-[url('/assets/auth.png')] bg-cover bg-bottom flex flex-col justify-center items-center flex-wrap bg-purple-200 min-h-screen gap-5">
+  
+    <h1 className='text-white text-3xl font-semibold '>Create Your Account</h1>
+   <div className='flex flex-col border-2 border-white rounded-xl p-3'>
+   <h1 className='text-white text-center font-bold text-2xl  mt-3'>Enter Your Credentials</h1>
    <form
-   className=" w-max  p-5 rounded-lg flex flex-col "
+   className=" p-10 w-[25rem] rounded-lg flex flex-col -mt-3 "
    onSubmit={handleSubmitForm}
    >
-  
+    <p><FaRegUser className='text-white' /></p>
       <input
         type="text"
         placeholder="enter name"
         required
         name="name"
-        className="border-gray-500 border-2"
+        className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         onChange={handleInputChange}
         value={newUser.name}
       ></input>
@@ -128,11 +129,9 @@ const RegisterUser = () => {
         onChange={handleInputChange}
         value={newUser.password}
       ></input>
-      <br />
-      <input type="file" name="image" onChange={handleFileChange} />
-      <br />
-      <br /> <br />
-      <button type="submit" className="border-gray-500 border-2">
+     <div className='flex justify-evenly mt-5'> <input type="checkbox" required/> <span className='text-white'>I agree to all the terms & conditions</span></div>
+     <div className='flex justify-center mt-3 text-md'>Already have an account ? &nbsp;<NavLink to="/user/login"><span className='text-white'>Login</span></NavLink></div>
+      <button type="submit" className="border-gray-500 border-2 mt-6">
         Register User
       </button>
     </form>
