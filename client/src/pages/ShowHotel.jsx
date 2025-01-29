3e566752f0424f1189f5c8ba543d1f2085760857
ANIKET SRIVASTAVA <aniket08official@gmail.com>
2025-01-29 22:09:55 +0530

    hotel page  space  enhanced !

--- client/src/pages/ShowHotel.jsx
+++ client/src/pages/ShowHotel.jsx
@@ -57,21 +57,21 @@ useEffect( () => {
   <Navbar/>
   </div>
   <div className="container ">
-  <h1 className="text-xl text-center  w-max m-auto shadow-black sm:text-3xl  md:text-4xl lg:text-5xl font-extrabold mt-8 mb-4 text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-pink-600">
+  <h1 className="text-xl text-center  w-max m-auto shadow-black sm:text-3xl  md:text-4xl lg:text-5xl font-extrabold mt-10 mb-10 text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-pink-600">
   Check out the Hotel details
   <span className="block mt-2 text-sm md:text-base text-red-500 font-semibold animate-bounce">
     Explore amazing amenities and features!
   </span>
 </h1>
 
-  <div className="mt-6 md:w-[70%] m-auto">
+  <div className="mb-20 mt-20 md:w-[65%] m-auto">
     <img
       src={showMyHotel.image}
       alt={showMyHotel.title}
       className=" rounded-xl  shadow-xl shadow-gray-900 object-contain"
     />
   </div>
-  <div className='flex justify-center items-center'>
+  <div className='border-red-500 border-2 my-20 flex justify-center items-center'>
   <ImageGallery />
   </div>
     <h2 className="text-3xl uppercase font-bold text-center mt-4">{showMyHotel.title}</h2>
@@ -79,16 +79,16 @@ useEffect( () => {
       {avgRating} ⭐ ({reviewCount} reviews)
     </p>
   <ul className=''>
-  <div className="max-w-5xl mx-auto px-4 py-8">
+  <div className="max-w-5xl mx-auto my-20 border-red-500 border-2 px-4 py-8">
       {/* Hotel Details Section */}
       <div className="bg-white shadow-md rounded-lg p-6 mb-8">
         <h1 className="text-2xl font-bold text-gray-800 mb-4">
           About Our Place:
         </h1>
-        <p className="text-gray-700 text-lg mb-4">
+        <p className="text-gray-700 text-justify text-lg mb-6">
           {showMyHotel.description}
         </p>
-        <p className="text-xl font-medium text-gray-800 mb-2">
+        <p className="text-xl font-medium text-gray-800 mb-6">
           For <span className="text-green-600 font-bold">Rs {(showMyHotel.price).toLocaleString("INR")} </span>
           <span className="text-sm text-gray-500 font-normal"> /- per night <br />
                   </span>
@@ -101,10 +101,12 @@ useEffect( () => {
       {/* Amenities Section */}
       <Amenities />
     </div>
-  <div className='w-[90%] m-auto border-3  shadow-md shadow-slate-600'><MapLocation hotel={showMyHotel} /></div>
+  <div className='border-red-500 border-2 my-60 w-[90%] m-auto border-3  shadow-md shadow-slate-600'><MapLocation hotel={showMyHotel} /></div>
   </ul>
+  <div className='border-2 border-red-500 my-40'>
   <Policies/>
-  <div className="flex justify-center items-center mt-8">
+  </div>
+  <div className="flex justify-center items-center">
       <Link to={`/hotel/${id}/booking`}>
         <button className="flex items-center justify-center text-xl bg-gradient-to-r from-pink-500 to-rose-500 text-white px-6 py-4 w-72 md:w-80 rounded-full font-bold shadow-lg hover:bg-gradient-to-r hover:from-red-500 hover:to-pink-500 hover:shadow-xl hover:scale-105 transition-all duration-300">
           <span className="group">Book Now</span>
@@ -113,16 +115,19 @@ useEffect( () => {
       </Link>
     </div>
 
-
-<div className=' gap-3 mt-5'>
+<div className='border-red-500 border-2 my-80'>
 <FAQs/>
+</div>
+<div className='border-red-500 border-2 my-20'>
 <Review/>
-<div className="flex flex-wrap gap-8 justify-center py-10 bg-gradient-to-b from-gray-50 to-white">
+</div>
+
+<div className="my-40 border-red-500 border-2 flex flex-wrap gap-8 justify-center py-10 bg-gradient-to-b from-gray-50 to-white">
   {reviews && reviews.length > 0 ? (
     reviews.map((reviewsItem) => (
       <div
         key={reviewsItem._id}
-        className="w-full sm:w-80 max-w-full bg-white shadow-lg rounded-2xl border border-gray-200 p-6 transition-transform transform hover:scale-105 hover:shadow-2xl"
+        className=" w-full sm:w-80 max-w-full bg-white shadow-lg rounded-2xl border border-gray-200 p-6 transition-transform transform hover:scale-105 hover:shadow-2xl"
       >
         {/* User Info */}
         <div className="flex items-center gap-4">
@@ -181,8 +186,8 @@ useEffect( () => {
 </div>
 
 
-  </div>
-  <div className='flex flex-col justify-center items-center space-y-7'>
+
+  <div className='my-20 flex flex-col justify-center items-center space-y-10'>
   <Link to="/">
   <button className="flex items-center justify-center space-x-5 bg-gradient-to-r from-purple-600 to-indigo-500 text-white px-6 py-3 w-80 rounded-full font-bold shadow-lg hover:bg-gradient-to-r hover:from-indigo-500 hover:to-purple-600 transform hover:scale-105 transition-all duration-300">
     <span>Home</span>
