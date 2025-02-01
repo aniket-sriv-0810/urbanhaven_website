02e334a387fee47295e057f2c17ce6c6bda0c3be
ANIKET SRIVASTAVA <aniket08official@gmail.com>
2025-02-01 22:31:02 +0530

    pages  updated !

--- server/controller/pages.controller.js
+++ server/controller/pages.controller.js
@@ -1,7 +1,7 @@
 import Contact from '../model/contact.model.js';
-import ApiError from '../utils/ApiError.js';
-import ApiResponse from '../utils/ApiResponse.js';
-import asyncHandler from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/ApiError.js';
+import {ApiResponse} from '../utils/ApiResponse.js';
+import {asyncHandler} from '../utils/asyncHandler.js';
 import {User} from '../model/user.model.js'
 const contactLogic = asyncHandler ( async ( req , res) => {
   try {
@@ -9,10 +9,11 @@ const contactLogic = asyncHandler ( async ( req , res) => {
      const checkUser = await User.findById(user);
     if( !checkUser)
         throw new ApiError (400 , "User not found !");
-    
+
       const contact = await Contact ({ user , message})
       await contact.save();
-      return res.json(200 , {contact} , "Feedback Saved successfully!");
+      return res.json(
+        new ApiResponse(200 , {contact} , "Feedback Saved successfully!"));
   } catch (error) {
     throw new ApiError(400 , error , "Unable to save feedback !");
   }
